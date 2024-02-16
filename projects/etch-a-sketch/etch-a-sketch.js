92173c0bdb2f2f611506fa2e3182d921fef7785e
w-ud <wahyudinyahya234@gmail.com>
2024-02-16 20:48:31 +0800

    Add file css and javascript to manipulate how much size of boxes needs to occupy the container evenly.

--- projects/etch-a-sketch/etch-a-sketch.js
+++ projects/etch-a-sketch/etch-a-sketch.js
@@ -1,1 +1,55 @@
-ECHO is on.
+const gridContainer = document.querySelector('#grid-container');
+const gridNumbers = 5;
+const gridContainer_width = 600;
+const gridContainer_height = 600;
+
+gridContainer.setAttribute('style', 
+  `max-width: ${gridContainer_width}px; 
+  height: ${gridContainer_height}px; 
+  backgroud-color: gray; 
+  border: 1px solid black; 
+  box-sixing: border-box; 
+  padding: 0;
+  display: flex;
+  margin: 0 auto;
+  `);
+
+
+for (let grid = 1; grid <= gridNumbers; grid++) {
+  const createdElement = document.createElement('div');
+  gridContainer.appendChild(createdElement);
+}
+
+const gridBoxes = document.querySelectorAll('#grid-container > div');
+gridBoxes.forEach((box) => {
+  box.setAttribute('style', 
+  `width: ${gridContainer_width / gridNumbers}px; 
+  height: 100%; 
+  border: 1px solid black; 
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  `);
+  box.classList.add('box');
+});
+
+
+const box = document.querySelectorAll('.box');
+
+box.forEach((boxIn) => {
+  for (let grid = 1; grid <= gridNumbers; grid++) {
+    const createdElement = document.createElement('div');
+    boxIn.appendChild(createdElement);
+  }
+});
+
+const boxIn = document.querySelectorAll('.box > div');
+
+boxIn.forEach((box) => {
+  box.setAttribute('style', 
+  `width: 100%; 
+  height: 100%; 
+  border: 1px solid black; 
+  flex: 1;
+  `);
+});
