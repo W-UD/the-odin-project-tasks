b9f04f5fe591c1614ca5d0bb1420307a8ecc2638
w-ud <wahyudinyahya234@gmail.com>
2024-02-17 08:21:23 +0800

    Add button to get input how many boxes user needs.

--- projects/etch-a-sketch/etch-a-sketch.js
+++ projects/etch-a-sketch/etch-a-sketch.js
@@ -1,13 +1,25 @@
+const addBoxesButton = document.querySelector('#add-boxes');
 const gridContainer = document.querySelector('#grid-container');
-const gridNumbers = 15;
+
+let gridNumbers = 5;
 const gridContainer_width = 600;
 const gridContainer_height = 600;
+let clicked = false;
 
-gridContainer.setAttribute('style', 
+addBoxesButton.addEventListener('click', () => {
+  const userInput = parseInt(prompt('Enter number of boxes you want..'));
+  gridNumbers = userInput;
+  gridContainer.innerHTML = '';
+  activateGrid();
+});
+
+
+function activateGrid() {
+  gridContainer.setAttribute('style', 
   `max-width: ${gridContainer_width}px; 
   height: ${gridContainer_height}px; 
   backgroud-color: gray; 
-  border: 1px solid black; 
+  border: 1px solid gray; 
   box-sixing: border-box; 
   padding: 0;
   display: flex;
@@ -15,57 +27,61 @@ gridContainer.setAttribute('style',
   `);
 
 
-for (let grid = 1; grid <= gridNumbers; grid++) {
-  const createdElement = document.createElement('div');
-  gridContainer.appendChild(createdElement);
-}
-
-const gridBoxes = document.querySelectorAll('#grid-container > div');
-gridBoxes.forEach((box) => {
-  box.setAttribute('style', 
-  `width: ${gridContainer_width / gridNumbers}px; 
-  height: 100%; 
-  border: 1px solid black; 
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  `);
-  box.classList.add('box');
-});
-
-
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach((boxIn) => {
   for (let grid = 1; grid <= gridNumbers; grid++) {
     const createdElement = document.createElement('div');
-    boxIn.appendChild(createdElement);
+    gridContainer.appendChild(createdElement);
   }
-});
 
-const boxIn = document.querySelectorAll('.box > div');
-
-boxIn.forEach((box) => {
-  box.setAttribute('style', 
-  `width: 100%; 
-  height: 100%; 
-  border: 1px solid black; 
-  flex: 1;
-  `);
-
-  box.setAttribute('class','box-child');
-});
-
-const boxChild = document.querySelectorAll('.box-child');
-boxChild.forEach((child) => {
-  child.addEventListener('mouseover', () => {
-    child.style.backgroundColor = 'blue';
-    //why set attribute here didnt work
-    //child.setAttribute('style', 'background-color: red');
+  const gridBoxes = document.querySelectorAll('#grid-container > div');
+  gridBoxes.forEach((box) => {
+    box.setAttribute('style', 
+    `width: ${gridContainer_width / gridNumbers}px; 
+    height: 100%; 
+    border: 1px solid gray; 
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    `);
+    box.classList.add('box');
   });
-  child.addEventListener('mouseout', () => {
-    child.style.backgroundColor = 'white';
-    //child.setAttribute('style', 'background-color: blue');
+
+
+  const boxes = document.querySelectorAll('.box');
+
+  boxes.forEach((boxIn) => {
+    for (let grid = 1; grid <= gridNumbers; grid++) {
+      const createdElement = document.createElement('div');
+      boxIn.appendChild(createdElement);
+    }
   });
-  
-});
+
+  const boxIn = document.querySelectorAll('.box > div');
+
+  boxIn.forEach((box) => {
+    box.setAttribute('style', 
+    `width: 100%; 
+    height: 100%; 
+    border: 1px solid gray; 
+    flex: 1;
+    `);
+
+    box.setAttribute('class','box-child');
+  });
+
+  const boxChild = document.querySelectorAll('.box-child');
+  boxChild.forEach((child) => {
+    child.addEventListener('mouseover', () => {
+      child.style.backgroundColor = 'blue';
+      //why set attribute here didnt work
+      //child.setAttribute('style', 'background-color: red');
+    });
+    child.addEventListener('mouseout', () => {
+      child.style.backgroundColor = 'white';
+      //child.setAttribute('style', 'background-color: blue');
+    });
+    
+  });
+}
+
+activateGrid();
+
