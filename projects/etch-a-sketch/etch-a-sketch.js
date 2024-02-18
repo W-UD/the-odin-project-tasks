f5d1e483a0aa68498149b0570205098b7c30f6ac
w-ud <wahyudinyahya234@gmail.com>
2024-02-18 11:45:07 +0800

    Add description button

--- projects/etch-a-sketch/etch-a-sketch.js
+++ projects/etch-a-sketch/etch-a-sketch.js
@@ -1,14 +1,41 @@
+const descriptionButton = document.querySelector('#description-button');
+const descriptions = document.querySelector('#descriptions');
 const addBoxesButton = document.querySelector('#add-boxes');
 const gridContainer = document.querySelector('#grid-container');
+const randomColorButton = document.querySelector('#to-randomize-color');
+const makeDarkerButton = document.querySelector('#make-darker');
+
+descriptions.style.display = 'none'
+let description = false;
 
 let gridNumbers = 5;
 const gridContainer_width = 600;
 const gridContainer_height = 600;
 let randomClicked = false;
+let darkerClicked = false;
+
+descriptionButton.addEventListener('click', () => {
+  if (!description) {
+    descriptionButton.textContent = "Descriptions: on";
+    descriptionButton.classList.add('clicked');
+    description = true;
+    descriptions.style.display = '';
+  }
+  else if (description) {
+    descriptionButton.textContent = "Descriptions: click to display";
+    descriptionButton.classList.remove('clicked');
+    description = false;
+    descriptions.style.display = 'none';
+  }
+});
 
 addBoxesButton.addEventListener('click', () => {
   const userInput = parseInt(prompt('Enter number of boxes you want..'));
-  if (userInput < 1 || userInput > 100) {
+  if (isNaN(userInput)) {
+    alert('You entered not a number. Please try it again');
+    return;
+  }
+  else if (userInput < 1 || userInput > 100) {
     alert('You entered a number that are not 1 to 100. please enter again');
     return;
   }
@@ -42,9 +69,10 @@ function activateGrid() {
     gridContainer.appendChild(createdElement);
   }
 
-  const gridBoxes = document.querySelectorAll('#grid-container > div');
-  gridBoxes.forEach((box) => {
-    box.setAttribute('style', `
+  const gridContainerChild = document.querySelectorAll('#grid-container > div');
+
+  gridContainerChild.forEach((gCChild) => {
+    gCChild.setAttribute('style', `
       width: ${gridContainer_width / gridNumbers}px; 
       height: 100%; 
       border: 1px solid gray; 
@@ -52,7 +80,7 @@ function activateGrid() {
       display: flex;
       flex-direction: column;
     `);
-    box.classList.add('box');
+    gCChild.classList.add('box');
   });
 
 
@@ -78,6 +106,7 @@ function activateGrid() {
   });
 
   const boxChild = document.querySelectorAll('.box-child');
+
   boxChild.forEach((child) => {
     child.style.backgroundColor = 'pink';
     child.addEventListener('mouseover', () => {
@@ -96,9 +125,9 @@ function activateGrid() {
 
   });
 
-  const randomColorButton = document.querySelector('#to-randomize-color');
   randomColorButton.addEventListener('click', (e) => {
     if (randomClicked === false) {
+      disableMakeDarker(boxChild);
       console.log(randomClicked);
       const clicked = randomColorButton.classList.add('clicked');
       randomColorButton.textContent = 'Random Color: on'
@@ -112,22 +141,17 @@ function activateGrid() {
     }
 
     else {
-      const clicked = randomColorButton.classList.remove('clicked');
-      randomColorButton.textContent = 'Random Color: disabled'
-      boxChild.forEach((child) => {
-        child.removeEventListener('click', getRandomColor);
-      });
-      randomClicked = false;
+     disabledRandomColor(boxChild);
     }
   });
 
-  const makeDarkerButton = document.querySelector('#make-darker');
   makeDarkerButton.addEventListener('click', () =>{
     makeDarker(boxChild);
   });
 
 }
 
+//Random Color Function
 function getRandomColor (e) {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
@@ -136,37 +160,63 @@ function getRandomColor (e) {
   const targetColor = e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
 
-
-function makeDarker(boxChild) {
-  console.log('darker');
-  let r = 251;
-  let g = 16;
-  let b = 55;
-
-  boxChild.forEach((child) => {
-    console.log('darker')
-      
-    child.addEventListener('click', (e) => {
-      if ((r < 30) && (g < 30) && (b < 30)) {
-        r = 251;
-        g = 16;
-        b = 55;
-        console.log('test');
-      }
-      e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-      r = r - (Math.floor(r * 10 / 100));
-      g = g - (Math.floor(g * 10 / 100));
-      b = b - (Math.floor(b * 10 / 100));
-      console.log(r, g, b);
+function disabledRandomColor(boxChild) {
+  const clicked = randomColorButton.classList.remove('clicked');
+    randomColorButton.textContent = 'Random Color: disabled'
+    boxChild.forEach((child) => {
+      child.removeEventListener('click', getRandomColor);
     });
-  });
+    randomClicked = false;
+}
+
+//Make Darker Function
+function makeDarker(boxChild) {
+  if (darkerClicked === false) {
+    disabledRandomColor(boxChild);
+    makeDarkerButton.textContent = "Make Darker: on";
+    const clicked = makeDarkerButton.classList.add('clicked');
+    darkerClicked = true;
+
+    boxChild.forEach((child) => {
+      console.log('darker')
   
+      child.addEventListener('click', activateMakeDarker);
+    });
+  }
+  else {
+    disableMakeDarker(boxChild);
+  }
+  
+}
+
+let r = 251;
+let g = 16;
+let b = 55;
+function activateMakeDarker(e) {
+  if ((r < 30) && (g < 30) && (b < 30)) {
+    r = 251;
+    g = 16;
+    b = 55;
+    console.log('test');
+  }
+  e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  r = r - (Math.floor(r * 10 / 100));
+  g = g - (Math.floor(g * 10 / 100));
+  b = b - (Math.floor(b * 10 / 100));
+  console.log(r, g, b);
+}
+
+function disableMakeDarker(boxChild) {
+  makeDarkerButton.textContent = "Make Darker: disabled";
+    boxChild.forEach((child) => {
+      console.log('darker')
+  
+      child.removeEventListener('click', activateMakeDarker);
+      makeDarkerButton.classList.remove('clicked');
+    });
+    darkerClicked = false;
 }
 
 activateGrid();
 
 //randomize box color
-
-
-
-
